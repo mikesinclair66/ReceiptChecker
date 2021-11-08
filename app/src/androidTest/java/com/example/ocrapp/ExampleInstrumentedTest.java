2dbf2d4ac2426004cf4676657129fd5d7a1f9269
Raziq Khan <35462830+RaziqK@users.noreply.github.com>
2021-11-08 13:46:51 -0800

    Basis for OCR

--- app/src/androidTest/java/com/example/ocrapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ocrapp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.receiptchecker;
+package com.example.ocrapp;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.receiptchecker", appContext.getPackageName());
+        assertEquals("com.example.ocrapp", appContext.getPackageName());
     }
 }
